9dd9899a0e26848230eb034b05c6919a425cba32
Victor <v.miguez2004@gmail.com>
2024-12-30 13:49:45 -0300

    refactor: plugin para limpar pasta build e rules para babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'transpiled', 'index.js'), // arquivo de entrada (arquivo onde está o ReactDOM.render)
+  entry: path.resolve(__dirname, 'src', 'index.js'), // arquivo de entrada (arquivo onde está o ReactDOM.createRoot)
   output: {
     path: path.resolve(__dirname, 'build'), // vai pra pasta build
     filename: 'bundle[hash].js' // nome do arquivo de saída
@@ -14,5 +14,14 @@ module.exports = {
       template: path.resolve(__dirname, 'public', 'index.html'), // mesmo diretório, pasta public, arquivo index.html
     }),
     new CleanWebpackPlugin() // plugin para limpar a pasta build
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // arquivos node_modules, já são transpilados antes de serem publicados
+        use: 'babel-loader', // usar o babel-loader para transpilar
+      },
+    ],
+  },
 };
