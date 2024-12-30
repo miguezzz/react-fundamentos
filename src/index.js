cfe6af7961868e0e94cc54f4da87bba2e3bc1c91
Victor <v.miguez2004@gmail.com>
2024-12-30 13:56:58 -0300

    refactor: update dev dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // cria a raiz do React a partir do id root
-root.render( // renderiza o conteúdo na raiz
-  <h1 id='title'>
-    <span>Hello World!!!</span>
-  </h1>, // html dentro do JS já é JSX (React sendo usado)
+const root = ReactDOM.createRoot(document.getElementById("root")); // cria a raiz do React a partir do id root
+root.render(
+	// renderiza o conteúdo na raiz
+	<h1 id="title">
+		<span>Hello World!</span>
+	</h1>, // html dentro do JS já é JSX (React sendo usado)
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
 // quem executa o webpack é o node, então o webpack.config.js é um arquivo node (usar require/exports)
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'), // arquivo de entrada (arquivo onde está o ReactDOM.createRoot)
-  output: {
-    path: path.resolve(__dirname, 'build'), // vai pra pasta build
-    filename: 'bundle[hash].js' // nome do arquivo de saída
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ // plugin para gerar o arquivo html na pasta build
-      template: path.resolve(__dirname, 'public', 'index.html'), // mesmo diretório, pasta public, arquivo index.html
-    }),
-    new CleanWebpackPlugin() // plugin para limpar a pasta build
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/, // arquivos node_modules, já são transpilados antes de serem publicados
-        use: 'babel-loader', // usar o babel-loader para transpilar
-      },
-    ],
-  },
+	entry: path.resolve(__dirname, "src", "index.js"), // arquivo de entrada (arquivo onde está o ReactDOM.createRoot)
+	output: {
+		path: path.resolve(__dirname, "build"), // vai pra pasta build
+		filename: "bundle[hash].js", // nome do arquivo de saída
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			// plugin para gerar o arquivo html na pasta build
+			template: path.resolve(__dirname, "public", "index.html"), // mesmo diretório, pasta public, arquivo index.html
+		}),
+		new CleanWebpackPlugin(), // plugin para limpar a pasta build
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/, // arquivos node_modules, já são transpilados antes de serem publicados
+				use: "babel-loader", // usar o babel-loader para transpilar
+			},
+		],
+	},
 };
