8a80b9032310537a2efd552a38e2c5f4a05f054d
Victor <v.miguez2004@gmail.com>
2024-12-30 19:10:36 -0300

    feat: Header and Post components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import Post from './Post';
+import Header from './Header';
+
 function App() {
   return (
     <>
-      <h1>Componente App importado!</h1>
-      <h2>Irmão de h1</h2>
+      <Header title="Vmigu's journal!">
+        <h2>Notícias da semana</h2> {/* children */}
+      </Header>
+
+      {/* <Post
+        title="vmigu recebe proposta de emprego"
+        subtitle="segundo fontes, vmigu é o mais novo employee da Amazon Web Services."
+      /> 
+      */}
+
+      <Post
+        post={{
+          // também dá pra passar um objeto: {} ou um array: []
+          title: 'vmigu recebe 29 propostas de emprego',
+          subtitle:
+            'segundo fontes, vmigu é o mais cobiçado employee das Big Techs!',
+        }}
+      />
     </> // short-syntax para React.Fragment. Componente que não renderiza nada, mas serve para agrupar elementos
   ); // por baixo dos panos: React.createElement('h1', null, 'Componente App importado!')
 }
