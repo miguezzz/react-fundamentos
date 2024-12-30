ff4d56921f37b9a693833f21b8233a2b78908d56
Victor <v.miguez2004@gmail.com>
2024-12-30 16:52:47 -0300

    componentes irmãos retornados dentro de uma tag parent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h1>Componente App importado!</h1>;
+  return (
+    <div>
+      <h1>Componente App importado!</h1>
+      <h2>Irmão de H1</h2>
+    </div> // componentes irmãos devem estar dentro de um componente pai
+  );
   // por baixo dos panos: React.createElement('h1', null, 'Componente App importado!')
 }
 
