8ee8d7f6ca96ff1fbc5630bdeaf7e68172ee7b15
Victor <v.miguez2004@gmail.com>
2025-01-05 21:17:24 -0300

    feat: css file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App'; // importa o componente App (o webpack vai transpilar o arquivo App.js para uma versão compatível com navegadores)
 
-// duas formas de criar componentes no React:
-// 1. Função
-// 2. Classe
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // cria a raiz do React a partir do id root lá no index.html da pasta public
 
