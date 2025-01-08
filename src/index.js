f411d4baba86d9617a67147f75386b4c34f616b3
Victor <v.miguez2004@gmail.com>
2025-01-08 00:46:18 -0300

    Global styled component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import GlobalStyle from './styles/global'; // importa o estilo global
+
 import App from './components/App'; // importa o componente App (o webpack vai transpilar o arquivo App.js para uma versão compatível com navegadores)
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // cria a raiz do React a partir do id root lá no index.html da pasta public
 
 // renderiza o conteúdo na raiz
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+);
