eb940bfb46e8915afedb0ec6c97e1bd9f188130e
Victor <v.miguez2004@gmail.com>
2025-01-09 11:21:41 -0300

    delete files

--- src/components/App/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-import MainTitle from '../Title';
-
-// estiliza a partir de outro componente estilizado
-export const Title = styled(MainTitle)`
-  color: rgb(231, 123, 0);
-`;
