8afb2690fdcf6a04e44643f6c408d9e03f418cd4
Victor <v.miguez2004@gmail.com>
2025-01-09 12:34:00 -0300

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from './styles/global';
 import Layout from './components/Layout';
 
+import themes from './styles/themes';
+
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={themes.light}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 }
