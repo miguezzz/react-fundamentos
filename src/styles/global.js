a4b36e173569290f4dfb6eee499d9fa9c126ccec
Victor <v.miguez2004@gmail.com>
2025-01-09 18:08:43 -0300

    comments

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  body {
+  body { // como o GlobalStyles está dentro de ThemeProvider no App.js, ele tem pode acessar o tema atual pelas props
     background: ${(props) => props.theme.backgroundColor};
     color: ${(props) => props.theme.textColor};
     font-family: sans-serif;
