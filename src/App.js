323ddcaddc80faf2127ae7033ece6d155da487e7
Victor <v.miguez2004@gmail.com>
2025-01-09 11:21:03 -0300

    App rendering layout, which is responsible for rendering Header, PostsList and Footer components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+import Layout from './components/Layout';
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Layout />
+    </>
+  );
+}
