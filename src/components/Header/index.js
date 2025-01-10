30bf3351ee71b9651916368ae89cd34a574f618b
Victor <v.miguez2004@gmail.com>
2025-01-10 19:42:28 -0300

    Refactor: Convert the Header component into a class component.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,41 @@
 import React, { useContext } from 'react';
-import { useTheme } from 'styled-components';
+// import { useTheme } from 'styled-components';
 
 import { Container } from './styles';
 
 import { MyThemeContext } from '../../contexts/MyThemeContext';
 
-export default function Header() {
-  const { theme, onToggleTheme } = useContext(MyThemeContext);
+export default class Header extends React.Component {
+  static contextType = MyThemeContext;
 
-  const usetheme = useTheme(); // hook useTheme para acessar o tema atual
-  console.log(usetheme);
+  render() {
+    const { theme, onToggleTheme } = this.context;
+    // se fossem props, seria: const { theme, onToggleTheme } = this.props;
+    // o this vem do extends React.Component. é um método de classe
 
-  return (
-    <Container>
-      <h1>Vmigu's Blog</h1>
-      <button type="button" onClick={onToggleTheme}>
-        {theme === 'dark' ? '🌞' : '🌑'}
-      </button>
-    </Container>
-  );
+    return (
+      <Container>
+        <h1>Vmigu's Blog</h1>
+        <button type="button" onClick={onToggleTheme}>
+          {theme === 'dark' ? '🌞' : '🌑'}
+        </button>
+      </Container>
+    );
+  }
 }
+
+// export default function Header() {
+//   const { theme, onToggleTheme } = useContext(MyThemeContext);
+
+//   const usetheme = useTheme(); // hook useTheme para acessar o tema atual
+//   console.log(usetheme);
+
+//   return (
+//     <Container>
+//       <h1>Vmigu's Blog</h1>
+//       <button type="button" onClick={onToggleTheme}>
+//         {theme === 'dark' ? '🌞' : '🌑'}
+//       </button>
+//     </Container>
+//   );
+// }
