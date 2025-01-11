566cd82d7b4c6167fec6d641887da92ea6399123
Victor <v.miguez2004@gmail.com>
2025-01-11 02:20:13 -0300

    refactor: move BrowserRouter to layout; feat: create navigation bar

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Posts from './pages/Posts';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <>
       <Route exact path="/" component={Home} />
       <Route exact path="/posts" component={Posts} />
-    </BrowserRouter>
+    </>
   );
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 import Header from '../Header';
 import Footer from '../Footer';
@@ -7,10 +8,14 @@ import Routes from '../../Routes';
 
 export default function Layout(props) {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+      </nav>
       <Routes />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
