0ebf8a7947477432b21b33488fc267658235612c
Victor <v.miguez2004@gmail.com>
2025-01-11 02:23:01 -0300

    style: Nav bar

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,1 +1,17 @@
 import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  background: #000;
+  margin-top: 16px;
+  padding: 16px;
+  border-radius: 4px;
+  a {
+    color: #fff;
+    text-decoration: none;
+    display: inline-block;
+
+    & + a { // todo link que tiver um link antes dele
+      margin-left: 16px;
+    }
+  } 
+`;
