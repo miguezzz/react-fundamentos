38e4215f9e94f3e5a84db89da96e35a72c516e4d
Victor Miguez <v.miguez2004@gmail.com>
2025-01-13 01:00:08 -0300

    feat: useHistory to redirect page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useTheme } from 'styled-components';
 
 import { Container } from './styles';
@@ -11,11 +12,20 @@ export default function Header() {
   const usetheme = useTheme(); // hook useTheme para acessar o tema atual
   console.log(usetheme);
 
+  const history = useHistory();
+
+  function handleNavigate() {
+    history.push('/posts');
+  }
+
   return (
     <Container>
       <h1>Vmigu's Blog</h1>
       <button type="button" onClick={onToggleTheme}>
         {theme === 'dark' ? '🌞' : '🌑'}
+      </button>
+      <button type="button" onClick={handleNavigate} style={{ color: '#fff' }}>
+        Navegar para posts
       </button>
     </Container>
   );
