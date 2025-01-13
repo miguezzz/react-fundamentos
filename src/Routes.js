c782b7164b74e588a3c211f50baa7592f5fb75d4
Victor Miguez <v.miguez2004@gmail.com>
2025-01-13 00:26:01 -0300

    feat: params and query params

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Posts from './pages/Posts';
+import Post from './pages/Post';
 import NotFound from './pages/NotFound';
 
 export default function Routes() {
@@ -11,6 +12,7 @@ export default function Routes() {
       {/* Switch component will only render the first child Route that matches the location */}
       <Route exact path="/" component={Home} />
       <Route exact path="/posts" component={Posts} />
+      <Route exact path="/posts/:id" component={Post} />
       <Route component={NotFound} />
     </Switch>
   );
--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
 
 export default function Post() {
+  const params = useParams();
+  console.log(params);
+
+  const { search } = useLocation();
+  const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+
+  console.log(queryParams.get('color'));
+
   return (
     <div>
       <h1>Post</h1>
