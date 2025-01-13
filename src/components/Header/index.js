b610aed8adb86f472103caef31ef6b7306262e24
Victor Miguez <v.miguez2004@gmail.com>
2025-01-13 00:37:08 -0300

    fix: useTheme import]

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-// import { useTheme } from 'styled-components';
+import { useTheme } from 'styled-components';
 
 import { Container } from './styles';
 
